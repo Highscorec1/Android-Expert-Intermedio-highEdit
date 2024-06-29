19b398f5741527850d96a9d671643e4a982b647c
Highscorec <geovajoel20@gmail.com>
2024-06-29 07:23:54 -0500

    dependencias bien

---
+++ app/src/main/java/dpmain/HoroscApp.kt
@@ -0,0 +1,8 @@
+package dpmain
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HoroscApp:Application() {
+}
--- app/src/main/java/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/ui/palmistry/PalmistryFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.ciberseguridadapp.R
 import com.example.ciberseguridadapp.databinding.FragmentPalmistryBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 
 class PalmistryFragment : Fragment() {
 
